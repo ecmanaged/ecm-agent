8dcb2249884fe874a2d0272c3cd9bbffe768d44f
Juan Carlos Moreno <juancarlos.moreno@ecmanaged.com>
2013-02-08 17:22:59 +0100

    - add script extension

--- plugins/script.py
+++ plugins/script.py
@@ -12,65 +12,58 @@ from os import chmod, environ
 
 import simplejson as json
 import stat
-	
+    
 class ECMScript(ECMPlugin):
-	def cmd_script_run(self, *argv, **kwargs):
-		script_base64		= kwargs.get('script',None)
-		script_executable	= kwargs.get('executable',None)
-		script_envars		= kwargs.get('envars',None)
-		
-		if not script_base64:
-			raise Exception("Invalid argument")
+    def cmd_script_run(self, *argv, **kwargs):
+        script_base64    = kwargs.get('script',None)
+        script_extension = kwargs.get('extension',None)
+        script_envars    = kwargs.get('envars',None)
 
-		try:
-			# Write down
-			tmp_dir = mkdtemp()
-			tmp_file = tmp_dir + '/script'
-			fh = open(tmp_file, "wb")
-			fh.write(b64decode(script_base64))
-			fh.close()
+        if not script_extension:
+            script_extension = '.cmd'
+        
+        if not script_base64:
+            raise Exception('Invalid argument')
 
-		except:
-			rmtree(tmp_dir, ignore_errors = True)
-			raise Exception("Unable to decode script")
+        try:
+            # Write down
+            tmp_dir = mkdtemp()
+            tmp_file = tmp_dir + '/script' + script_extension
+            fh = open(tmp_file, "wb")
+            fh.write(b64decode(script_base64))
+            fh.close()
 
+        except:
+            raise Exception("Unable to decode script")
 
-		cmd = []
-		if script_executable: 
-			# Add executable to command
-			cmd = split(script_executable)
-		else:
-			# Set as executable by owner if not explicit executable
-			chmod(tmp_file, stat.S_IEXEC)
-			
-		# Add temp file as last argument (or first if not executable)
-		cmd.append(tmp_file)
-			
-		# Set environment variables before execution
-		try:
-			if script_envars:
-				script_envars = b64decode(script_envars)
-				script_envars = json.loads(script_envars)
-				for envar in script_envars:
-					if not script_envars[envar]: script_envars[envar] = ''
-					environ[envar] = str(script_envars[envar])
-		except:
-			# Ignore it
-			pass
-		
-		# Execute but don't try/catch to get real error
-		p = Popen(cmd,
-			stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=tmp_dir)
-		stdout, stderr = p.communicate()
+        # Set script executable
+        chmod(tmp_file, stat.S_IEXEC)
 
-		ret = {}
-		ret['out'] = p.wait()
-		ret['stdout'] = str(stdout)
-		ret['stderr']  = str(stderr)
-		
-		# Clean
-		rmtree(tmp_dir, ignore_errors = True)
-		return ret
+        # Set environment variables before execution
+        try:
+            if script_envars:
+                script_envars = b64decode(script_envars)
+                script_envars = json.loads(script_envars)
+                for envar in script_envars:
+                    if not script_envars[envar]: script_envars[envar] = ''
+                    environ[envar] = str(script_envars[envar])
+        except:
+            # Ignore it
+            pass
+        
+        # Execute but don't try/catch to get real error
+        p = Popen(tmp_file,
+            stdin=PIPE, stdout=PIPE, stderr=PIPE, cwd=tmp_dir)
+        stdout, stderr = p.communicate()
+
+        ret = {}
+        ret['out'] = p.wait()
+        ret['stdout'] = str(stdout)
+        ret['stderr']  = str(stderr)
+        
+        # Clean
+        rmtree(tmp_dir, ignore_errors = True)
+        return ret
 
 
 ECMScript().run()
