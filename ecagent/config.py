9ed4fbb6313c998407e8785914cceaee37461e13
Arindam Choudhury <arindam@live.com>
2015-07-17 17:26:27 +0200

    Update config.py

--- ecagent/config.py
+++ ecagent/config.py
@@ -128,6 +128,12 @@ class SMConfigObj(ConfigObj):
 
         if not auth_content:
             auth_content = yield getPage(auth_url_alt)
+            
+        #////////////////////////////FIX FOR AMAZON AMI SSL HANDSHAKE ERROR///////////////
+        if not auth_content:
+            import urllib2
+            auth_content = urllib2.urlopen(auth_url)
+        #///////////////////////////END OF FIX///////////////////////////////////////////
 
         for line in auth_content.splitlines():
             if line and line.startswith('uuid:'):
