babf6592d45f4e37007c69cd98e725e4fb3093a4
Arindam Choudhury <arindam@live.com>
2015-08-26 18:18:59 +0200

    added pip installer: single package

--- plugins/__helper.py
+++ plugins/__helper.py
@@ -274,6 +274,106 @@ def packagekit_install_single_package(package):
 
     return False
 
+def pip_install_single_package(package, site_wide):
+    '''packagke: package name
+       side_wide: boolean. if True package will be installed in site packages, else in user site
+    '''
+    import logging
+
+    from pip.index import PackageFinder
+    from pip.req import InstallRequirement, RequirementSet
+    from pkg_resources import safe_name
+    from setuptools.package_index import distros_for_url
+    from pip.download import PipSession
+    from pip.exceptions import DistributionNotFound, BestVersionAlreadyInstalled
+    from pip._vendor.packaging.version import parse
+    from pip.locations import src_prefix, site_packages, user_site
+
+    logging.basicConfig()
+
+    session = PipSession()
+
+        # site_wide is a boolean. if True package will be installed in site packages, else in user site
+    install_site_wide = site_wide
+
+    pkg = package
+
+    pkg_normalized = safe_name(pkg).lower()
+    req = InstallRequirement.from_line(pkg, None)
+    pf = PackageFinder(find_links=[], index_urls=['https://pypi.python.org/simple/'], session=session)
+
+    try:
+        req.populate_link(pf, True)
+    except BestVersionAlreadyInstalled:
+        print 'Best version already installed'
+    except DistributionNotFound:
+        print 'No matching distribution found for '+pkg
+
+    if req.check_if_exists():
+        print 'already installed'
+        print 'satisfied by: '+str(req.satisfied_by)
+        print 'conflicts with:'+str(req.conflicts_with)
+
+        # check if update available
+        update_version = None
+
+        if req.link.is_wheel:
+            from pip.wheel import Wheel
+            update_version = Wheel(req.link.filename).version
+        else:
+            for dist in distros_for_url(req.link.url):
+                if safe_name(dist.project_name).lower() == pkg_normalized and dist.version:
+                    update_version = dist.version
+                break
+        if not update_version:
+            print 'Could not obtain the updated version number'
+            exit()
+        if parse(update_version) > parse(req.installed_version):
+            print "update available"
+            if install_site_wide:
+                reqset = RequirementSet(build_dir=site_packages, src_dir=src_prefix, download_dir=None, session=session, use_user_site=False, upgrade= True)
+            else:
+                reqset = RequirementSet(build_dir=user_site, src_dir=src_prefix, download_dir=None, session=session, use_user_site=True, upgrade= True)
+            reqset.add_requirement(req)
+            print reqset.build_dir
+            print reqset.src_dir
+            reqset._check_skip_installed(req, pf)
+            reqset.prepare_files(pf)
+            if install_site_wide:
+                reqset.install(install_options=[], global_options=[])
+            else:
+                reqset.install(install_options=['--user'], global_options=[])
+            reqset.cleanup_files()
+            print 'updated'
+        else:
+            print 'installed version is up-to-date'
+
+    else:
+        print 'not installed'
+
+        if install_site_wide:
+            reqset = RequirementSet(build_dir=site_packages, src_dir=src_prefix, download_dir=None, session=session, use_user_site=False)
+        else:
+            reqset = RequirementSet(build_dir=user_site, src_dir=src_prefix, download_dir=None, session=session, use_user_site=True)
+
+        reqset.add_requirement(req)
+        print reqset.build_dir
+        print reqset.src_dir
+
+        reqset.prepare_files(pf)
+
+        if install_site_wide:
+            reqset.install(install_options=[], global_options=[])
+        else:
+            reqset.install(install_options=['--user'], global_options=[])
+        reqset.cleanup_files()
+
+        if req.install_succeeded:
+            req.check_if_exists()
+            print 'installation done'
+            print 'satisfied by: '+str(req.satisfied_by)
+            print 'conflicts with:'+str(req.conflicts_with)
+
 # def apt_install_package(package):
 #     if type(package) is types.StringType:
 #         apt_install_single_package(package)
