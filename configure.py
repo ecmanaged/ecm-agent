8ae2e7bab977ee44ec6860d8b8e3a492a13f4bae
Arindam Choudhury <arindam@live.com>
2016-08-12 13:10:38 +0200

    Add groups to ECM Agent

--- configure.py
+++ configure.py
@@ -33,11 +33,15 @@ if "." not in sys.path:
 
 configure_account = None
 configure_groups = None
+configure_add_groups = None
+configure_delete_groups = None
 
 try:
-    optlist, args = getopt.getopt(sys.argv[1:], 'a:g:', ["account=", "groups="])
+    optlist, args = getopt.getopt(sys.argv[1:], 'a:g:ag:dg', ["account=", "groups=", "add-groups=", "delete-groups="])
 except getopt.GetoptError:
-    print 'Please configure agent with ./configure.py --account=XXXXX --groups=XXXXXX'
+    print 'Please configure agent with ./configure.py --account=XXXXX --groups=abc,def'
+    print 'You can add groups using --add-groups=xyz'
+    print 'You can delete groups using --delete-groups=xyz'
     sys.exit(-1)
 
 for option, value in optlist:
@@ -47,9 +51,11 @@ for option, value in optlist:
     elif option in ("-g", "--groups"):
         configure_groups = value
 
-if not configure_account and not configure_groups:
-    print 'Please configure agent with ./configure.py --account=XXXXX --groups=XXXXXX'
-    sys.exit(-1)
+    elif option in ("-ag", "--add-groups"):
+        configure_add_groups = value
+
+    elif option in ("-dg", "--delete-groups"):
+        configure_delete_groups = value
 
 root_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(root_dir)
@@ -69,12 +75,47 @@ if not os.path.isfile(config_file):
 
 config = ECMConfigObj(config_file)
 
+if not configure_account and not config['Auth']['account']:
+    print 'Please configure agent with ./configure.py --account=XXXXX'
+    sys.exit(-1)
+
 if configure_account:
     config['Auth']['account'] = configure_account
 
 if configure_groups:
-    for group in configure_groups.split(','):
-        config['Groups'][group] = ''
+    list_groups = configure_groups.split(',')
+    list_groups = map(str.strip, list_groups)
+    groups = ','.join(list_groups)
+    config['Groups']['groups'] = groups
+
+if configure_add_groups:
+    groups = config['Groups']['groups']
+    list_groups = groups.split(',')
+    list_groups = map(str.strip, list_groups)
+
+    add_groups = configure_add_groups.split(',')
+    add_groups = map(str.strip, add_groups)
+
+    for group in add_groups:
+        if group not in list_groups:
+            list_groups.append(group)
+
+    groups_string = ','.join(list_groups)
+    config['Groups']['groups'] = groups_string
+
+if configure_delete_groups:
+    groups = config['Groups']['groups']
+    list_groups = groups.split(',')
+    list_groups = map(str.strip, list_groups)
+
+    del_groups = configure_delete_groups.split(',')
+    del_groups = map(str.strip, del_groups)
+
+    for group in del_groups:
+        if group in list_groups:
+            list_groups.remove(group)
+    groups_string = ','.join(list_groups)
+    config['Groups']['groups'] = groups_string
 
 config.write()
 print 'Manual configuration override succeeded.'
